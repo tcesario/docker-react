280479003dc49220fe451b31e4d1c3dbfa405140
root <thiago.csilva94@gmail.com>
2020-03-21 19:46:07 +0000

    text changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  I was changed the text message!
+	  It was changed the text message!
         </p>
         <a
           className="App-link"
